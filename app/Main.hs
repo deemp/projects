04f547331be4db178633a4e7ae97189c501a7234
br4ch1st0chr0n3 <br4ch1st0chr0n3@gmail.com>
2022-04-27 12:17:19 +0300

    update config

--- app/Main.hs
+++ app/Main.hs
@@ -5,11 +5,13 @@ module Main where
 import Network.Wreq                  -- package wreq
 import Control.Lens                  -- package lens
 import qualified Data.ByteString.Lazy as BL
+import System.Directory (createDirectory)
 
 main = do
   -- make a requeset
   r <- get "https://www.google.com/robots.txt"
   -- get the contents (as a lazy ByteString)
   let contents = r ^. responseBody
+  createDirectory "./repos"
   -- write it to a local file
   BL.writeFile "./repos/myfile.txt" contents
