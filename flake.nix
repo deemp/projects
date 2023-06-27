b68792a83dcf9cd43602e0921caea2dfeae8b0bc
Danila Danko <br4ch1st0chr0n3@gmail.com>
2023-06-27 10:05:41 +0300

    set up workflow

--- flake.nix
+++ flake.nix
@@ -5,48 +5,41 @@
     codium.url = "github:deemp/flakes?dir=codium";
     flake-utils_.url = "github:deemp/flakes?dir=source-flake/flake-utils";
     flake-utils.follows = "flake-utils_/flake-utils";
-    vscode-extensions_.url = "github:deemp/flakes?dir=source-flake/nix-vscode-extensions";
-    vscode-extensions.follows = "vscode-extensions_/vscode-extensions";
     devshell.url = "github:deemp/flakes?dir=devshell";
     flakes-tools.url = "github:deemp/flakes?dir=flakes-tools";
+    drv-tools.url = "github:deemp/flakes?dir=drv-tools";
     workflows.url = "github:deemp/flakes?dir=workflows";
   };
   outputs = inputs: inputs.flake-utils.lib.eachDefaultSystem
     (system:
       let
         pkgs = inputs.nixpkgs.legacyPackages.${system};
-        inherit (inputs.codium.functions.${system}) mkCodium writeSettingsJSON;
-        inherit (inputs.codium.configs.${system}) extensions settingsNix;
-        inherit (inputs.vscode-extensions.extensions.${system}) vscode-marketplace open-vsx;
-        inherit (inputs.devshell.functions.${system}) mkCommands mkRunCommands mkShell;
-        inherit (inputs.workflows.functions.${system}) writeWorkflow;
-        inherit (inputs.workflows.configs.${system}) nixCI;
-        inherit (inputs.flakes-tools.functions.${system}) mkFlakesTools;
+        inherit (inputs.codium.lib.${system}) mkCodium writeSettingsJSON;
+        inherit (inputs.codium.lib.${system}) extensions extensionsCommon settingsNix settingsCommonNix;
+        inherit (inputs.devshell.lib.${system}) mkCommands mkRunCommands mkShell;
+        inherit (inputs.workflows.lib.${system}) writeWorkflow;
+        inherit (inputs.workflows.lib.${system}) nixCI;
+        inherit (inputs.flakes-tools.lib.${system}) mkFlakesTools;
+        inherit (inputs.drv-tools.lib.${system}) mkShellApps;
 
-        tools = [ pkgs.hello ];
+        scripts = (mkShellApps {
+          genDocs = {
+            text = ''
+              (cd hs-notes && ${pkgs.nix}/bin/nix run .#genDocs)
+              ${pkgs.mdbook}/bin/mdbook build docs
+            '';
+            description = "Generate docs";
+          };
+        });
 
         packages = {
           # --- IDE ---
 
-          # This part can be removed if you don't use `VSCodium`
-          # We compose `VSCodium` with dev tools
-          # This is to let `VSCodium` run on its own, outside of a devshell
-          codium = mkCodium {
-            extensions = {
-              inherit (extensions) nix haskell misc github markdown;
-              # We can include more extensions by providing them in an attrset here
-              extra = {
-                inherit (vscode-marketplace.golang) go;
-              };
-            };
-            runtimeDependencies = tools;
-          };
+          # VSCodium with extensions
+          codium = mkCodium { extensions = extensionsCommon; };
 
           # a script to write `.vscode/settings.json`
-          writeSettings = writeSettingsJSON {
-            inherit (settingsNix) todo-tree files editor gitlens
-              git nix-ide workbench markdown-all-in-one markdown-language-features;
-          };
+          writeSettings = writeSettingsJSON settingsCommonNix;
 
           # --- Flakes ---
 
@@ -69,18 +62,30 @@
           # --- GH Actions
 
           # A script to write GitHub Actions workflow file into `.github/ci.yaml`
-          writeWorkflows = writeWorkflow "ci" nixCI;
-        };
+          writeWorkflows =
+            import ./nix-files/workflow.nix {
+              name = "ci";
+              inherit scripts system;
+              inherit (inputs) workflows;
+            };
+        } // scripts;
+
+        tools = [
+          pkgs.mdbook
+        ];
 
         devShells.default = mkShell {
           packages = tools;
-          bash.extra = "hello";
           commands =
-            mkCommands "tools" tools
-            ++ mkRunCommands "ide" {
+            mkCommands "tools" tools ++
+            mkRunCommands "ide" {
               "codium ." = packages.codium;
               inherit (packages) writeSettings;
-            };
+            } ++
+            mkRunCommands "docs" {
+              inherit (packages) genDocs;
+            }
+          ;
         };
       in
       {
@@ -89,13 +94,11 @@
 
   nixConfig = {
     extra-trusted-substituters = [
-      "https://haskell-language-server.cachix.org"
       "https://nix-community.cachix.org"
       "https://cache.iog.io"
       "https://deemp.cachix.org"
     ];
     extra-trusted-public-keys = [
-      "haskell-language-server.cachix.org-1:juFfHrwkOxqIOZShtC4YC1uT1bBcq2RSvC7OMKx0Nz8="
       "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="
       "hydra.iohk.io:f/Ea+s+dFdN+3Y/G+FDgSq+a5NEWhJGzdjvKNGv0/EQ="
       "deemp.cachix.org-1:9shDxyR2ANqEPQEEYDL/xIOnoPwxHot21L5fiZnFL18="
---
+++ nix-files/workflow.nix
@@ -0,0 +1,45 @@
+{ workflows, system, name, scripts }:
+let
+  inherit (workflows.lib.${system})
+    writeWorkflow expr mkAccessors genAttrsId
+    steps run os nixCI installNix cacheNix;
+  job1 = "_1_update_flake_locks";
+  job2 = "_2_front";
+  names = mkAccessors {
+    secrets = genAttrsId [ "GITHUB_TOKEN" ];
+  };
+  workflow =
+    nixCI // {
+      jobs = {
+        "${job1}" = nixCI.jobs.nixCI;
+        "${job2}" =
+          {
+            name = "Publish static files";
+            permissions = {
+              contents = "write";
+            };
+            runs-on = os.ubuntu-20;
+            steps = [
+              steps.checkout
+              (installNix { })
+              (cacheNix { keySuffix = "static"; })
+              {
+                name = "Build docs";
+                run = run.nixScript { name = scripts.genDocs.pname; };
+              }
+              {
+                name = "GitHub Pages action";
+                uses = "peaceiris/actions-gh-pages@v3.9.3";
+                "with" = {
+                  github_token = expr names.secrets.GITHUB_TOKEN;
+                  publish_dir = "./docs";
+                  force_orphan = true;
+                };
+              }
+              steps.nixStoreCollectGarbage
+            ];
+          };
+      };
+    };
+in
+writeWorkflow name workflow
