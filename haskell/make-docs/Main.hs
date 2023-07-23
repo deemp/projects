cb31d6b1b113c4e876c4f20ea75c8eb50866233c
Danila Danko <br4ch1st0chr0n3@gmail.com>
2023-07-23 20:57:24 +0300

    don't use state

--- haskell/make-docs/Main.hs
+++ haskell/make-docs/Main.hs
@@ -16,7 +16,7 @@
 
 module Main (main) where
 
-import Control.Lens (At (at), itraverse_, use, (%=), (&), (+=), (.=), (<&>), (<>=), (?~), (^.))
+import Control.Lens (At (at), itraverse_, (%=), (&), (<&>), (<>=), (?~), (^.))
 import Control.Monad.State
 import Converter (Format (Hs, Md), convertTo, dedent, def, disable, enable, indent)
 import Data.List (intercalate, sort)
@@ -31,7 +31,7 @@ import Data.Traversable (for)
 import Data.Yaml (FromJSON, decodeFileThrow)
 import GHC.Generics (Generic)
 import System.Directory (createDirectoryIfMissing, doesDirectoryExist, doesFileExist, listDirectory)
-import System.Environment (getArgs, getEnv)
+import System.Environment (getEnv)
 import System.FilePath (dropExtension, isExtensionOf, joinPath, splitPath)
 
 newtype Path = Path {_path :: FilePath} deriving newtype (Eq, IsString)
@@ -41,52 +41,42 @@ instance Show Path where
 
 data NodeSubDir = NodeSubDir {path :: Path, children :: Map.Map String NodeSubDir} deriving (Eq, Show)
 
-addPath :: [String] -> Path -> NodeSubDir -> State Int NodeSubDir
-addPath [] path n = error "can't construct a node without a module name."
-addPath (x : xs) finalPath currentNode = do
-  childNode <- case xs of
-    [] -> pure NodeSubDir{path = finalPath, children = Map.empty}
+addPath :: [String] -> Path -> NodeSubDir -> NodeSubDir
+addPath [] _ _ = error "can't construct a node without a module name."
+addPath (x : xs) finalPath currentNode = res
+ where
+  childNode = case xs of
+    [] -> NodeSubDir{path = finalPath, children = Map.empty}
     xs_ ->
       case currentNode.children ^. at x of
-        Nothing -> do
-          idx <- use id
-          id += 1
-          -- TODO enable path = [i|dummy/#{idx}|]?
-          addPath xs_ finalPath NodeSubDir{path = [i||], children = Map.empty}
-        Just n -> do
-          addPath xs_ finalPath n
-  pure NodeSubDir{children = currentNode.children & at x ?~ childNode, path = currentNode.path}
+        Nothing -> addPath xs_ finalPath NodeSubDir{path = "", children = Map.empty}
+        Just n -> addPath xs_ finalPath n
+  res = NodeSubDir{children = currentNode.children & at x ?~ childNode, path = currentNode.path}
 
 data NodeProject = NodeProject {heading :: Text, nodes :: [NodeSubDir]}
 
 mkNodesProject :: FilePath -> [Project] -> [NodeProject]
 mkNodesProject prefix projects =
-  flip evalState 0 $
-    for
-      projects
-      ( \Project{..} -> do
-          nodes <-
-            for
-              subDirs
-              ( \SubDir{..} -> do
-                  cnt <- use id
-                  let (res_, cnt_) = flip execState (NodeSubDir{children = Map.empty, path = ""}, cnt) $
-                        for hsModules $ \hsModule -> do
-                          let xs = splitOn "/" hsModule
-                          id %= \(n, c) -> runState (addPath xs (Path [i|#{prefix}/#{mdPrefix}/#{hsModule}.md|]) n) c
-                  id .= cnt_
-                  pure res_
-              )
-          pure NodeProject{heading, nodes}
-      )
+  projects
+    <&> \Project{..} ->
+      let
+        nodes =
+          subDirs
+            <&> \SubDir{..} ->
+              flip execState NodeSubDir{children = Map.empty, path = ""} $
+                for hsModules $ \hsModule -> do
+                  let xs = splitOn "/" hsModule
+                  id %= \n -> addPath xs (Path [i|#{prefix}/#{mdPrefix}/#{hsModule}.md|]) n
+       in
+        NodeProject{heading, nodes}
 
 pPrintNodeSubDir :: Int -> NodeSubDir -> State [Text] ()
 pPrintNodeSubDir depth t =
   itraverse_
     ( \idx child -> do
-        id <>= [[i|#{concat (replicate depth " ")}- [#{idx}](#{path child})|]]
+        id <>= [[i|#{concat (replicate (depth * 2) " ")}- [#{idx}](#{path child})|]]
         case child of
-          NodeSubDir{} -> pPrintNodeSubDir (depth + 2) child
+          NodeSubDir{} -> pPrintNodeSubDir (depth + 1) child
     )
     t.children
 
@@ -100,11 +90,6 @@ pPrintNodeProject t =
 
 pPrintProjects :: FilePath -> [Project] -> Text
 pPrintProjects prefix ns = Text.intercalate "\n\n" $ pPrintNodeProject <$> mkNodesProject prefix ns
-
-newtype M = M Text
-instance Show M where
-  show :: M -> String
-  show (M s) = Text.unpack s
 
 data Project = Project {heading :: Text, subDirs :: [SubDir]} deriving (Generic, Show)
 data SubDir = SubDir {hsPrefix :: FilePath, mdPrefix :: FilePath, hsModules :: [FilePath]} deriving (Generic, Show)
