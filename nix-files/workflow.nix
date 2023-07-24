3ee511ccb67ed76be971f13ea37c713d15d2dcc7
Danila Danko <br4ch1st0chr0n3@gmail.com>
2023-07-24 12:28:46 +0300

    upd workflow

--- nix-files/workflow.nix
+++ nix-files/workflow.nix
@@ -16,31 +16,27 @@ let
             macosMaxStoreSize = 15000000000;
           };
           doCommit = false;
-          steps = _: stepsIf "${names.matrix.os} == '${os.ubuntu-22}'" [
-            (
-              let nameUpdateDocs = "Update docs"; in
-              [
-                {
-                  name = nameUpdateDocs;
-                  run = run.nixScript { name = scripts.genDocs.pname; };
-                }
-                (steps.commit { messages = [ (steps.updateLocks { }).name (steps.format { }).name nameUpdateDocs ]; })
-              ]
-            )
-            {
-              name = "Copy docs";
-              run = "cp -r docs/book docs/dist";
-            }
-            {
-              name = "Publish docs to GitHub Pages";
-              uses = "peaceiris/actions-gh-pages@v3.9.3";
-              "with" = {
-                github_token = expr names.secrets.GITHUB_TOKEN;
-                publish_dir = "docs/dist";
-                force_orphan = true;
-              };
-            }
-          ];
+          steps = { stepsAttrs, ... }:
+            stepsIf "${names.matrix.os} == '${os.ubuntu-22}'" [
+              {
+                name = "Update docs";
+                run = run.nixScript { name = scripts.genDocs.pname; };
+              }
+              (steps.commit { messages = [ (steps.updateLocks { }).name (steps.format { }).name stepsAttrs."Update docs".name ]; })
+              {
+                name = "Copy docs";
+                run = "cp -r docs/book docs/dist";
+              }
+              {
+                name = "Publish docs to GitHub Pages";
+                uses = "peaceiris/actions-gh-pages@v3.9.3";
+                "with" = {
+                  github_token = expr names.secrets.GITHUB_TOKEN;
+                  publish_dir = "docs/dist";
+                  force_orphan = true;
+                };
+              }
+            ];
         };
       };
 in
