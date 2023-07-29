7f0f1ae31b08e8240dd276e73f598bf9da48b1b7
Danila Danko <br4ch1st0chr0n3@gmail.com>
2023-07-29 22:16:45 +0300

    refactor stack.nix

--- haskell/manager/stack.nix
+++ haskell/manager/stack.nix
@@ -1,12 +1,3 @@
-{ ghcVersion ? "902" }: (
-  (import
-    (
-      let lock = builtins.fromJSON (builtins.readFile ./flake.lock); in
-      fetchTarball {
-        url = "https://github.com/edolstra/flake-compat/archive/${lock.nodes.flake-compat.locked.rev}.tar.gz";
-        sha256 = lock.nodes.flake-compat.locked.narHash;
-      }
-    )
-    { src = ./.; }
-  ).defaultNix
-).outputs.stack-shell.${builtins.currentSystem} { inherit ghcVersion; }
+{ ghcVersion ? "928" }:
+let default = ((builtins.getFlake "github:deemp/flakes/${(builtins.fromJSON (builtins.readFile ../flake.lock)).nodes.flakes.locked.rev}").outputs.makeDefault ./.);
+in default.outputs.stack-shell.${builtins.currentSystem} { inherit ghcVersion; }
